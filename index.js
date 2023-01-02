b6925655496ed37a4fa17f537a040600339a3424
Simon <simon1240.se@gmail.com>
2023-01-02 17:40:55 +0100

    final (it is)

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-const currentYear = new Date().getFullYear();
-const currentDate = new Date();
-const startDate = new Date(currentDate.getFullYear(), 0, 1);
-const endDate = new Date(currentDate.getFullYear(), 11, 31);
+// Get all elements that need changing
+const title = document.getElementsByTagName('title')[0];
+const mainText = document.getElementsByClassName('mainText');
+const loadinBar = document.getElementsByClassName('loadingBar');
 
-let progress = (currentDate - startDate) / (endDate - startDate)	;
+function update() {
+	// Update vars
+	let currentYear = new Date().getFullYear();
+	let currentDate = new Date();
+	let startDate = new Date(currentDate.getFullYear(), 0, 1);
+	let endDate = new Date(currentDate.getFullYear(), 11, 31);
+	// Calculate progress
+	let progress = (currentDate - startDate) / (endDate - startDate)	;
+	// Edit elements with new values
+	title.innerHTML = "Year " + currentYear + " (" + progress.toFixed(2) + "%)";
+	mainText[0].innerHTML = currentYear;
+	loadinBar[0].style.width = progress + "%"
+	// Some info for myself in the console
+	console.clear()
+	console.log(
+		'Year = '+currentYear+ '\n' +
+		'Date = '+currentDate+  '\n' +
+		'Progress = '+progress+  '\n' +
+		'Clean Progress = '+progress.toFixed(2) + '%');
+	}
 
-let title = document.getElementsByTagName('title')[0];
-title.innerHTML = "Year " + currentYear + " (" + progress.toFixed(2) + "%)";
-
-let mainText = document.getElementsByClassName('mainText');
-mainText[0].innerHTML = currentYear;
-
-let loadinBar = document.getElementsByClassName('loadingBar');
-loadinBar[0].style.width = progress + "%"
+// Call the update Function every Second
+(function(){
+    update();
+    setTimeout(arguments.callee, 1000);
+})();
